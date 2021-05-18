75c0125a4e2f50e60bc1c17141192414856d42f5
岡優志 <yu.j.0513@gmail.com>
2021-05-18 20:46:19 +0900

    全てのコンポーネントに命名しました

--- Kadai2BySB/Kadai2BySB/ViewController.swift
+++ Kadai2BySB/Kadai2BySB/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputTextFieldOne: UITextField!
+    @IBOutlet weak var inputTextFieldTwo: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func segmentedControl(_ sender: UISegmentedControl) {
+        
+    }
+   
+    @IBAction func resultButton(_ sender: UIButton) {
+    }
+    
 
 }
 
