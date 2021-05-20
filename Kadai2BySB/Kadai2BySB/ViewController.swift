340118a248d3adc6b6ae147463eacd22e4b11a0d
岡優志 <yu.j.0513@gmail.com>
2021-05-20 22:36:09 +0900

    enumによる責務の切り出しを試しました

--- Kadai2BySB/Kadai2BySB/ViewController.swift
+++ Kadai2BySB/Kadai2BySB/ViewController.swift
@@ -29,16 +29,16 @@ class ViewController: UIViewController {
          Index番号によって計算式を変更する。 case(3,0)に関してはTextFieldTwoに0が入るとエラーになるので必ずcase(3,_)より手前で処理
          defaultにはエラー処理
          */
-        switch (segmentedControl.selectedSegmentIndex, numAraay[1]) {
-        case (0,_):
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
             resultLabel.text = String(numAraay[0] + numAraay[1])
-        case (1,_):
+        case 1:
             resultLabel.text = String(numAraay[0] - numAraay[1])
-        case (2,_):
+        case 2:
             resultLabel.text = String(numAraay[0] * numAraay[1])
-        case (3,0):
+        case 3 where numAraay[1] == 0:
             resultLabel.text = "0"
-        case (3,_):
+        case 3:
             resultLabel.text = String(numAraay[0] / numAraay[1])
         default:
             resultLabel.text = "error"
@@ -46,3 +46,41 @@ class ViewController: UIViewController {
     }
 }
 
+private enum Operator {
+    enum Error: Swift.Error {
+        case divisionByZero
+    }
+
+    case addition
+    case subtraction
+    case multiplication
+    case division
+    
+    init?(selectedSegmentIndex: Int) {
+        switch selectedSegmentIndex {
+        case 0: self = .addition
+        case 1: self = .subtraction
+        case 2: self = .multiplication
+        case 3: self = .division
+        default: return nil
+        }
+    }
+    
+    func calculate(ope:Operator, num1:Int, num2:Int) -> Result<Int, Error> {
+        func apply(_ ope: (Int,Int) -> Int) -> Result<Int, Error> {
+            .success(ope(num1, num2))
+        }
+        switch ope {
+               case .addition:
+                   return apply(+)
+               case .subtraction:
+                   return apply(-)
+               case .multiplication:
+                   return apply(*)
+               case .division where num1 == 0:
+                return .failure(.divisionByZero)
+               case .division:
+                   return apply(/)
+               }
+    }
+}
